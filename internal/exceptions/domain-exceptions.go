a093d6a0201f7758e414a984a967915c8af29c12
charmingruby <mrrubycanal3105@gmail.com>
2024-01-16 12:53:45 -0300

    feat: domain layer exceptions

---
+++ internal/exceptions/domain-exceptions.go
@@ -0,0 +1,43 @@
+package exceptions
+
+import (
+	"fmt"
+
+	"github.com/charmingruby/clize/helpers"
+)
+
+type GenericValidationError struct {
+	Message string
+}
+
+func (e *GenericValidationError) Error() string {
+	return e.Message
+}
+
+type RequiredFieldError struct {
+	FieldName string
+}
+
+func (e *RequiredFieldError) Error() string {
+	msg := fmt.Sprintf("%s is required", e.FieldName)
+	return msg
+}
+
+type FieldLengthError struct {
+	FieldName      string
+	IsMinimumError bool
+	Quantity       int
+}
+
+func (e *FieldLengthError) Error() string {
+	minMsg := fmt.Sprintf("%s must be at least %d characters", e.FieldName, e.Quantity)
+	maxMsg := fmt.Sprintf("%s must be a maximum of %d characters", e.FieldName, e.Quantity)
+
+	msg := helpers.If[string](
+		e.IsMinimumError,
+		minMsg,
+		maxMsg,
+	)
+
+	return msg
+}
